2be4ad52c2969a087b90a3361b7c7705fa24803e
Gee-Bee <greg.bunia@gmail.com>
2020-04-02 15:09:05 +0200

    bundle gem

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrawnExamples
+
+These are code examples from official [Prawn manual](http://prawnpdf.org/manual.pdf)
---
+++ lib/prawn_examples.rb
@@ -0,0 +1,6 @@
+require "prawn_examples/version"
+
+module PrawnExamples
+  class Error < StandardError; end
+  # Your code goes here...
+end
